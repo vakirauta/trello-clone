7580a9fbac8bbdc4645bc8cf0504f4ee5c34274d
Vakirauta <Tarabas1989@mail.ru>
2019-11-17 21:00:39 +0300

    --- Auto-Git Commit ---

---
+++ column.js
@@ -0,0 +1,64 @@
+const Column = {
+    IdCounter: 4,
+
+    process (columnElement) {
+        const spanAction_addNote = columnElement.querySelector('[data-action-addNote]')
+        spanAction_addNote.addEventListener('click', function (event) {
+            const noteElement = document.createElement('div')
+                noteElement.classList.add('note')
+                noteElement.setAttribute('draggable', 'true')
+                noteElement.setAttribute('data-note-id', Note.idCounter)
+
+            Note.idCounter++
+
+            columnElement.querySelector('[data-notes]').append(noteElement)
+            Note.process(noteElement);
+            
+            noteElement.setAttribute('contenteditable', 'true')
+            noteElement.focus()
+
+        })
+
+        const headerElement = columnElement.querySelector('.column-header')
+
+        headerElement.addEventListener('dblclick', function (event) {
+            headerElement.setAttribute('contenteditable', true)
+            headerElement.focus()
+        })
+
+        headerElement.addEventListener('blur', function (event) {
+            headerElement.removeAttribute('contenteditable', true)
+        })
+
+        columnElement.addEventListener('dragover', function (event) {
+            event.preventDefault()
+        })
+
+        columnElement.addEventListener('drop', function (event) {
+            if (Note.dragged) {
+               return  columnElement.querySelector('[data-notes]').append(Note.dragged)
+            }
+        })
+    },
+    
+    columnCreate (event) {
+        const columnElement = document.createElement('div')
+            columnElement.classList.add('column')
+            columnElement.setAttribute('draggable', 'true')
+            columnElement.setAttribute('data-column-id', Column.IdCounter)
+
+            columnElement.innerHTML = 
+            `<p class="column-header">В плане</p>
+            <div data-notes></div>
+            <p class="column-footer">
+                <span data-action-addNote class="action">+ Добавить карточку</span>
+            </p>`
+
+            Column.IdCounter++
+
+            document.querySelector('.columns').append(columnElement)
+
+            Column.process (columnElement)
+
+    }
+}
--- script.js
+++ script.js
@@ -1,78 +1,17 @@
 
-let columnIdCounter = 4;
-
-
     document
         .querySelectorAll('.column')
-        .forEach(columnProcess)
+        .forEach(Column.process)
 
     document
         .querySelector('[data-action-addColumn]')
-        .addEventListener ('click', function(event) {
-            const columnElement = document.createElement('div')
-            console.log()
-                columnElement.classList.add('column')
-                columnElement.setAttribute('draggable', 'true')
-                columnElement.setAttribute('data-column-id', columnIdCounter)
-
-                columnElement.innerHTML = 
-                `<p class="column-header">В плане</p>
-                <div data-notes></div>
-                <p class="column-footer">
-                    <span data-action-addNote class="action">+ Добавить карточку</span>
-                </p>`
-
-                columnIdCounter++
-
-                document.querySelector('.columns').append(columnElement)
-
-                columnProcess (columnElement)
-
-        })
+        .addEventListener ('click', Column.columnCreate)
 
     document
         .querySelectorAll('.note')
         .forEach(Note.process)
 
-function columnProcess (columnElement) {
-        const spanAction_addNote = columnElement.querySelector('[data-action-addNote]')
-        spanAction_addNote.addEventListener('click', function (event) {
-            const noteElement = document.createElement('div')
-                noteElement.classList.add('note')
-                noteElement.setAttribute('draggable', 'true')
-                noteElement.setAttribute('data-note-id', Note.idCounter)
 
-            Note.idCounter++
-
-            columnElement.querySelector('[data-notes]').append(noteElement)
-            Note.process(noteElement);
-            
-            noteElement.setAttribute('contenteditable', 'true')
-            noteElement.focus()
-
-        })
-
-        const headerElement = columnElement.querySelector('.column-header')
-
-        headerElement.addEventListener('dblclick', function (event) {
-            headerElement.setAttribute('contenteditable', true)
-            headerElement.focus()
-        })
-
-        headerElement.addEventListener('blur', function (event) {
-            headerElement.removeAttribute('contenteditable', true)
-        })
-
-        columnElement.addEventListener('dragover', function (event) {
-            event.preventDefault()
-        })
-
-        columnElement.addEventListener('drop', function (event) {
-            if (Note.dragged) {
-               return  columnElement.querySelector('[data-notes]').append(Note.dragged)
-            }
-        })
-    }
 
 
     
